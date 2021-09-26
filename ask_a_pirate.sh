6cb838270fb9f212ac50bd1677791679b40432c6
Robin Engler <robin.engler@sib.swiss>
2021-09-26 22:23:33 +0200

    Port-Royal towncentre

---
+++ ask_a_pirate.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+source .script/quest_functions.sh
+
+# Quest starts here...
+pirate_say "Sail Ho! Welcome to Port-Royal, the pirate's city.\n"
+sleep 2
+echo -e "Here you will find anything you need... if you have enough gold!\n"
+sleep 3
+
+echo "Rhum? Yes, can do!"
+sleep 3
+
+echo "I heard they are storing it in the Tavern's backyard... "
+sleep 3
+echo -e "...maybe you could have a look there at night and steal some.\n"
+sleep 3
+
+echo "But don't forget: if you get caught... never mention me!"
+
+exit 0
